2d1c1f414b854ad1d6f55a8686631ea7d8c4476a
ZashQash <zeeshqamar@gmail.com>
2015-12-22 23:56:16 +0530

    Revert "Revert "Simple and Medium Done""
    This reverts commit 8bd7c1c3f898b7e6007b093b8cd5e10f5efa337d.

---
+++ Client Simple/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client_Simple
+{
+    class Program
+    {
+        private static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint locaEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"),1234);
+
+            try
+            {
+                sck.Connect(locaEndPoint);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to connect remote end point");
+                Main(args);
+            }
+
+            Console.WriteLine("Enter Text: ");
+            string text = Console.ReadLine();
+            byte[] data = Encoding.Default.GetBytes(text);
+
+            sck.Send(data);
+            Console.WriteLine("Data Sent!");
+            Console.WriteLine("Press Any key Continue");
+            Console.Read();
+            sck.Close();
+        }
+    }
+}
---
+++ Server Simple/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server_Simple
+{
+    class Program
+    {
+        static byte[] Buffer { get; set; }
+        private static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //sck.Bind(new IPEndPoint(IPAddress.Any, 1234));
+            sck.Bind(new IPEndPoint(0,1234));
+            sck.Listen(100);
+
+            //Blocking call, wait for unavailbe socket attempt to connect and transfer the availbale socket to accept
+            Socket accepted = sck.Accept();
+            //Default buffer size 8192, That is maximum no. of byte could receive at one time
+            Buffer = new byte[accepted.SendBufferSize];
+            //No. of byte read, Receiving data waiting in the buffer and transfer to Buffer
+            int byteRead = accepted.Receive(Buffer);
+            byte[] formated = new byte[byteRead];
+            //To remove the Garbage bytes
+            for (int i = 0; i < byteRead; i++)
+            {
+                formated[i] = Buffer[i];
+            }
+            string strData = Encoding.ASCII.GetString(formated);
+            Console.WriteLine(strData);
+            Console.Read();
+            sck.Close();
+        }
+    }
+}
