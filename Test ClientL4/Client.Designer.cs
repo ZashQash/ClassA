cb2b25fd0a35c1cfe106d3e207b573d7b042c143
ZashQash <zeeshqamar@gmail.com>
2015-12-27 16:28:02 +0530

    Avoided Shadab disaster ;-)

---
+++ Test ClientL4/Client.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Test_ClientL4
+{
+    partial class frmClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.rtbDispaly = new System.Windows.Forms.RichTextBox();
+            this.btnSendText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(0, 366);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(353, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // rtbDispaly
+            // 
+            this.rtbDispaly.Location = new System.Drawing.Point(0, 1);
+            this.rtbDispaly.Name = "rtbDispaly";
+            this.rtbDispaly.Size = new System.Drawing.Size(353, 363);
+            this.rtbDispaly.TabIndex = 1;
+            this.rtbDispaly.Text = "";
+            // 
+            // btnSendText
+            // 
+            this.btnSendText.Location = new System.Drawing.Point(0, 394);
+            this.btnSendText.Name = "btnSendText";
+            this.btnSendText.Size = new System.Drawing.Size(353, 23);
+            this.btnSendText.TabIndex = 2;
+            this.btnSendText.Text = "Send Text";
+            this.btnSendText.UseVisualStyleBackColor = true;
+            this.btnSendText.Click += new System.EventHandler(this.btnSendText_Click);
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 421);
+            this.Controls.Add(this.btnSendText);
+            this.Controls.Add(this.rtbDispaly);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "frmClient";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox rtbDispaly;
+        private System.Windows.Forms.Button btnSendText;
+    }
+}
+
---
+++ Test ClientL4/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Test_ClientL4
+{
+    public partial class frmClient : Form
+    {
+        public frmClient()
+        {
+            InitializeComponent();
+        }
+
+        Socket sck;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1990);
+            
+            try
+            {
+                sck.Connect(serverEndPoint);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fail to Connect Server", "Client");
+                Application.Exit();
+            }
+        }
+
+        private void btnSendText_Click(object sender, EventArgs e)
+        {
+            byte[] buffer = Encoding.Default.GetBytes(rtbDispaly.Text);
+            byte[] byteSize = BitConverter.GetBytes(buffer.Length);
+            sck.Send(byteSize, 0, 4, 0);
+            sck.Send(buffer, 0, buffer.Length, 0);
+        }
+    }
+}
