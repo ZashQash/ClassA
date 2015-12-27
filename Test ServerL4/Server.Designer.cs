2b277181dba25a2ce04a5a5b1dc2b6da3a64fd35
ZashQash <zeeshqamar@gmail.com>
2015-12-27 12:03:34 +0530

    Server L2 is done not debuged yet

---
+++ Test ServerL4/Server.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Test_ServerL4
+{
+    partial class frmServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.rtbDisplay = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(-3, 415);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(456, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "Listen";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // rtbDisplay
+            // 
+            this.rtbDisplay.Location = new System.Drawing.Point(-3, 4);
+            this.rtbDisplay.Name = "rtbDisplay";
+            this.rtbDisplay.Size = new System.Drawing.Size(455, 405);
+            this.rtbDisplay.TabIndex = 1;
+            this.rtbDisplay.Text = "";
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 437);
+            this.Controls.Add(this.rtbDisplay);
+            this.Controls.Add(this.btn);
+            this.Name = "frmServer";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.RichTextBox rtbDisplay;
+    }
+}
+
---
+++ Test ServerL4/Server.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.Win32.SafeHandles;
+using System.IO;
+
+namespace Test_ServerL4
+{
+    public partial class frmServer : Form
+    {
+        public frmServer()
+        {
+            InitializeComponent();
+        }
+
+        Socket sock;
+        Socket accept;
+        private void btn_Click(object sender, EventArgs e)
+        {
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Bind(new IPEndPoint(IPAddress.Any, 1990));
+            sock.Listen(0);
+
+            accept = sock.Accept();
+            sock.Close();
+
+            MemoryStream ms = new MemoryStream();
+
+            #region ThreadStart
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    byte[] sizebyte = new byte[4];
+                    accept.Receive(sizebyte, 0, sizebyte.Length, 0);
+                    int size = BitConverter.ToInt32(sizebyte, 0);
+
+                    while (size > 0)
+                    {
+                        byte[] buffer;
+                        
+                        buffer = size < accept.ReceiveBufferSize ? new byte[size] : new byte[accept.ReceiveBufferSize];
+                        //if (size < accept.ReceiveBufferSize)
+                        //{
+                        //    buffer = new byte[size];
+                        //}
+                        //else
+                        //{
+                        //    buffer = new byte[accept.ReceiveBufferSize];
+                        //}
+
+                        int recByte = accept.Receive(buffer, 0, buffer.Length, 0);
+                        size -= recByte;
+                        ms.Write(buffer, 0, buffer.Length);
+                    }
+                    ms.Close();
+                    byte[] data = ms.ToArray();
+                    ms.Dispose();
+
+                    Invoke((MethodInvoker) delegate
+                    {
+                        rtbDisplay.Text = Encoding.Default.GetString(data);
+                    });
+                }
+            }).Start();
+            #endregion
+        }
+    }
+}
